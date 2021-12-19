73bd2a88686ef336323573885824d1edfb563b1e
Romex24 <paulinab2014@interia.pl>
2021-12-19 22:23:44 +0100

    Morse pierwsze testy

---
+++ morse.py
@@ -0,0 +1,5 @@
+def Morse(word):
+    if word == 'a' or word == 6:
+        return 'Wrong Character'
+    if word == '.-':
+        return "a"
---
+++ test_morse.py
@@ -0,0 +1,14 @@
+import unittest
+import re
+from morse import Morse 
+
+class TestMorse(unittest.TestCase):
+    def test_input(self):
+        self.assertEqual(Morse('a'), "Wrong Character")
+        self.assertEqual(Morse(6), "Wrong Character")
+        self.assertEqual(Morse('.-'), "a")
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
